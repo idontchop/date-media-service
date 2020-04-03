61cebb34f731c7a1980c992d4743b9280dde1133
Nate D <nate@idontchop.com>
2020-04-02 22:12:04 -0700

    Initial commit

---
+++ src/main/java/com/idontchop/datemediaservice/DateMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.datemediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateMediaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateMediaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idontchop/datemediaservice/DateMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idontchop.datemediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateMediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
